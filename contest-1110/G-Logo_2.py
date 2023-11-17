1a58f7b92588d3659b150f12de7ce70b7252d3bd
Cristiano <cristianosampaio16@gmail.com>
2023-11-16 22:59:58 -0300

    add logo 2 problem

--- contest-1110/G-Logo_2.py
+++ contest-1110/G-Logo_2.py
@@ -9,6 +9,21 @@ def get_y_projection(vet_len, angle):
     return vet_len * math.sin(math.radians(angle))
 
 
+def update_positions(command, number, x_point, y_point, current_degree):
+    if command == "fd":
+        x_point += get_x_projection(number, current_degree)
+        y_point += get_y_projection(number, current_degree)
+    elif command == "bk":
+        x_point -= get_x_projection(number, current_degree)
+        y_point -= get_y_projection(number, current_degree)
+    elif command == "lt":
+        current_degree += number
+    elif command == "rt":
+        current_degree -= number
+
+    return x_point, y_point, current_degree
+
+
 tests_number = int(input())
 while tests_number:
     tests_number -= 1
@@ -18,75 +33,44 @@ while tests_number:
     x_point = 0
     y_point = 0
     current_degree = 0
-    degree_1 = 0
-    degree_2 = 0
 
-    polygon_size = 0
+    after_lost_commands = []
+    is_after_lost_command = False
+
     for i in range(commands_number):
         command, number = input().split()
+        if is_after_lost_command:
+            after_lost_commands.append((command, number))
         if number == "?":
+            is_after_lost_command = True
             lost_command = command
             lost_degree = current_degree
+            x_lost = x_point
+            y_lost = y_point
         else:
-            number = int(number)
+            x_point, y_point, current_degree = update_positions(
+                command, int(number), x_point, y_point, current_degree
+            )
 
-            if command == "fd":
-                polygon_size += 1
-                x_point += get_x_projection(number, current_degree)
-                y_point += get_y_projection(number, current_degree)
-            elif command == "bk":
-                polygon_size += 1
-                x_point -= get_x_projection(number, current_degree)
-                y_point -= get_y_projection(number, current_degree)
-            elif command == "lt":
-                degree_1 += number
-                degree_2 += 180 - number
-                current_degree += number
-            elif command == "rt":
-                degree_1 += 180 - number
-                degree_2 += number
-                current_degree -= number
-
+    # If the lost command is to move uses the distance of the previous problem
     if lost_command == "fd" or lost_command == "bk":
         print(round(math.sqrt(x_point**2 + y_point**2)))
+    # If the lost command is to rotate, the 360 options are tested,
+    # from the point where the command was lost
     else:
-        if round(math.sqrt(x_point**2 + y_point**2)) == 0:
-            print(0)
-        else:
-            # elif polygon_size > 2:
-            #     internal_angle = (polygon_size - 2) * 180
+        for number in range(0, 360):
+            x_point, y_point, current_degree = update_positions(
+                lost_command, number, x_lost, y_lost, lost_degree
+            )
+            for i in range(len(after_lost_commands)):
+                x_point, y_point, current_degree = update_positions(
+                    after_lost_commands[i][0],
+                    int(after_lost_commands[i][1]),
+                    x_point,
+                    y_point,
+                    current_degree,
+                )
 
-            #     # print("internal_angle", internal_angle)
-            #     # print("degree_1", degree_1)
-            #     # print("degree_2", degree_2)
-
-            #     internal_option = internal_angle - degree_1
-            #     internal_option2 = internal_angle - degree_2
-            #     # print("internal_option", internal_option)
-            #     # print("internal_option2", internal_option2)
-
-            #     if degree_2 + 360 - internal_option == 360:
-            #         # print("degree_1 interno")
-            #         # print((360 - degree_2) // 2)
-            #     # else:
-            #         # print("degree_2 interno")
-            #         # print((360 - degree_1) // 2)
-            # else:
-            # print("banban")
-
-            print("final --> ", lost_command, 360 - (current_degree % 360))
-# 5
-# fd 100
-# lt 120
-# fd 100
-# rt ?
-# fd 100
-
-
-# 6
-# fd 3
-# rt 90
-# fd 4
-# rt 90
-# rt ?
-# fd 5
+            if round(math.sqrt(x_point**2 + y_point**2)) == 0:
+                break
+        print(number)
